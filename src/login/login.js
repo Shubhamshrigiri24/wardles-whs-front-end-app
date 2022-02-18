630f65dd59dfbe5410d1fec427be610231d04cc9
EshanWellDigital <98742923+EshanWellDigital@users.noreply.github.com>
2022-02-18 12:56:17 +0530

    updated_login_functionality
    created login api method using dummy api

--- src/login/login.js
+++ src/login/login.js
@@ -1,17 +1,37 @@
 import { Grid,Paper, TextField, Button, Typography } from '@material-ui/core';
 // import Errormessage from "./Error";
+import React, { useState, useEffect } from 'react'
 // import {Link} from 'react-router-dom' ;
-import DoneIcon from '@mui/icons-material/Done';
-import InputAdornment from '@mui/material/InputAdornment';
-import { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom'
 function Login(){ 
-    const [data, setData] = useState([])
     const [email, setEmail] = useState("")
-    
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (localStorage.getItem('user-info')) {
+            navigate.push("/contact")
+        
+        }
+    }, [])
+    async function login(){
+    console.warn("data", email, password)
+    let item = { email, password };
+    let result = await fetch('http://localhost:3333/login',{
+        method: 'POST',
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json" 
+        },
+        body: JSON.stringify(item)
+    });
+    result = await result.json();
+    localStorage.setItem('user-info', JSON.stringify(result))
+    navigate.push("/contact")
 
+    }
     const paperStyle={padding :20, width:340, margin:"10px auto", background: "#F7FBFF"};
 
+   
 
     return(
         <Grid>
@@ -23,24 +43,15 @@ function Login(){
                     <p style={{fontSize: 18,marginBottom:15}}>Enter your email address and password.</p>
 
                 <TextField size="small"
-                    label="Email address"
+                    label="Email address" onChange={(e)=>setEmail(e.target.value)}
                     style={{marginBottom:"17px",boxShadow: "2px 2px 7px rgba(0, 0, 0, 0.07)"}}
                     placeholder="Email address" 
                     type="email"  variant="outlined" 
                     fullWidth
-                    onChange={e => setEmail(e.target.value)}
-                    // data.find(email => email === email)
-                    InputProps={{
-                        startAdornment: (
-                          <InputAdornment position="end">
-                           {  true ? <DoneIcon style={{ color: "green" }} /> :  null }
-                          </InputAdornment>
-                        ),
-                      }}
                     />
                 <TextField 
-                    size="small"
-                    label="Password"
+                    size="small" 
+                    label="Password" onChange={(e) => setPassword(e.target.value)}
                     style={{marginBottom:"17px",boxShadow: "2px 2px 7px rgba(0, 0, 0, 0.07)"}} 
                     placeholder="Password" 
                     type="Password"
@@ -54,10 +65,10 @@ function Login(){
                 </Typography>
 
 
-                <Button variant="contained" size="Large" disableElevation style={{background: "#FFCD00",color:"#07283C",  fontSize:"18px", textTransform: "none"}} fullWidth type="submit" >Sign In</Button>
+                <Button variant="contained" onClick={login} size="Large" disableElevation style={{background: "#FFCD00",color:"#07283C",  fontSize:"18px", textTransform: "none"}} fullWidth type="submit" >Sign In</Button>
 
-                <Typography style={{marginTop:"17px",textAlign: "Center"}}>
-                    <a href='# ' style={{fontSize: 17,color:"#0066BE",textDecoration:"none"}}>Don't have an account yet?</a> 
+                <Typography  style={{marginTop:"17px",textAlign: "Center"}}>
+                    <a href='# ' style={{fontSize: 17,color:"#0066BE",textDecoration:"none"}}>Dont have an account yet?</a> 
                 </Typography> 
 
 
